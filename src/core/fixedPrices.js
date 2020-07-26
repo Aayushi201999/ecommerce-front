9028cf3dcc2736104cf85eacb68366150d82c79e
Aayushi201999 <aayushi.jaiswal.che17@itbhu.ac.in>
2020-07-26 20:20:25 +0530

    commit

---
+++ src/core/fixedPrices.js
@@ -0,0 +1,38 @@
+export const prices=[
+    {
+        _id:0,
+        name: "Any",
+        array: []
+    },
+    {
+        _id:1,
+        name: "Rs.0 to Rs.200",
+        array: [0,200]
+    },
+    {
+        _id:2,
+        name: "Rs.201 to Rs.400",
+        array: [201,400]
+    },
+    {
+        _id:3,
+        name: "Rs.401 to Rs.600",
+        array: [401,600]
+    },
+    {
+        _id:4,
+        name: "Rs.601 to Rs.800",
+        array: [601,800]
+    },
+    {
+        _id:5,
+        name: "Rs.801 to 1000",
+        array: [801,1000]
+    },
+    {
+        _id:6,
+        name: "more than 1000",
+        array: [1001,10000]
+    }
+    
+]
